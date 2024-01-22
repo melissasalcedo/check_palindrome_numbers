6b0a5bb9e3ae4690889f3a0a6060f442aa324d2c
melissasalcedo <salcedomelissa189@gmil.com>
2024-01-22 20:46:09 +0800

    Finished

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,5 +1,27 @@
 # Check if the numbers is palindrome
-
 # Pseudocode
 # Function
+def palindrome(number):
+    orig_num = number
+
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+    return orig_num == reverse_num
+
 # Numbers
+number1 = 12345
+number2 = 899898
+number3 = 1010101
+
+result1 = palindrome(number1)
+print(f"{number1} is a palindrome: {result1}")
+
+result2 = palindrome(number2)
+print(f"{number2} is a palindrome: {result2}")
+
+result3 = palindrome(number3)
+print(f"{number1} is a palindrome: {result3}")
+
